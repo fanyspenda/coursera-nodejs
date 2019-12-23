e556c478b2bd8ab82e7b17d1bc83ee4bb3243753
Fany Ervansyah <fanyervansyah.9c@gmail.com>
2019-12-23 22:35:57 +0700

    week 1: express router

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const http = require("http");
 const morgan = require(`morgan`);
+const bodyParser = require("body-parser");
 
 const hostname = "localhost";
 const port = 3000;
 
+const dishesRouter = require(`./routes/dishesRouter`);
+
 const app = express();
-app.use(morgan(`dev`));
+app.use(morgan(`dev`)); //memberikan feedback pada CLI jika ada request dari browser
+app.use(bodyParser.json());
+
+//ketika mengakses /dishes, akan merouting ke dishes router.
+app.use("/dishes", dishesRouter);
 
 //mereturn file html sesuai url
 app.use(express.static(__dirname + `/public`));
@@ -17,7 +24,7 @@ app.use((req, res, next) => {
   res.end(`
     <html>
       <body>
-        <h1>Ini Express server</h1>
+        <h1>Ini servernya Express</h1>
       </body>
     </html>
   `);
